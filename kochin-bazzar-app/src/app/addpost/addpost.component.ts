ffa096d6d5196043607ad32e06637f4e9394cb0a
suryanarayananpalathole <nsurya537@gmail.com>
2023-02-07 11:00:07 +0530

    new

--- kochin-bazzar-app/src/app/addpost/addpost.component.ts
+++ kochin-bazzar-app/src/app/addpost/addpost.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import AWSS3UploadAshClient from 'aws-s3-upload-ash';
 import { UploadResponse } from 'aws-s3-upload-ash/dist/types';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-addpost',
@@ -13,38 +14,22 @@ export class AddpostComponent implements OnInit {
 
   
   
-  constructor() { }
+  constructor(public http : HttpClient) { }
 
   ngOnInit(): void {
   }
-  addpostForm = new FormGroup({
-    title: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)]),
-    location: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)]),
-    price: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(15)]),
-    type: new FormControl('',[Validators.required]),
-    email: new FormControl('',[Validators.email,Validators.required]),
-    mobile: new FormControl('',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
-    details: new FormControl('',[Validators.required]),
-    image:new FormControl('',[Validators.required]),
-
-  });
-
   // upload image if file value changed
-  imagefile: any;
+  imagefile: any=null;
   imgname:any;
-
   config = {
     bucketName: 'post-imageskochin',
     dirName: 'media', /* optional - when use: e.g BUCKET_ROOT/dirName/fileName.extesion */
     region: 'ap-northeast-1',
-    accessKeyId:"AKIA3EOB7Q47HCSMYEW2",
-    secretAccessKey:"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" ,
+    accessKeyId:environment.AWS_ACCESS_KEY,
+    secretAccessKey: environment.AWS_SECRET_KEY,
     s3Url: 'https://post-imageskochin.s3.ap-northeast-1.amazonaws.com/'
   }
   S3CustomClient: AWSS3UploadAshClient = new AWSS3UploadAshClient(this.config);
-
-
-
 
   selectimg(file:any){
     this.imagefile=file.target.files[0]
@@ -55,11 +40,54 @@ export class AddpostComponent implements OnInit {
 
   }
   async handleSendFile() {
+    console.log(environment)
+    console.log("File sent")
+    await this.S3CustomClient
+      .uploadFile(this.imagefile, this.imagefile.type, undefined, this.imagefile.name, "public")
+      .then((data: UploadResponse) => console.log(data))
+      .catch((err: any) => console.error(err) )}
 
-    console.log("handleSendFile")
-    await this.S3CustomClient.uploadFile(this.imagefile, this.imagefile.type, undefined, this.imagefile.name, "private")
-      .then((data: UploadResponse) => {console.log(data)
-      console.log("uploading")})
-      .catch((err: any) => console.error(err))}
+
+
+  addpostForm = new FormGroup({
+    title: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)]),
+    location: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)]),
+    price: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(15)]),
+    type: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.email,Validators.required]),
+    mobile: new FormControl('',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+    details: new FormControl('',[Validators.required]),
+    image:new FormControl('',[Validators.required]),
+
+  }); 
+
+  
+      send(){
+        console.log(this.addpostForm.get("title")?.value+" " + 
+        this.addpostForm.get("location")?.value+" " + 
+        this.addpostForm.get("price")?.value+" " + 
+        this.addpostForm.get("type")?.value+""+
+        this.addpostForm.get("email")?.value+""+
+        this.addpostForm.get("mobile")?.value+""+
+        this.addpostForm.get("details")?.value
+        )
+    
+        this.http.post("http://localhost:8084/post/postadd",{
+              postName:this.addpostForm.get("title")?.value,
+          postLocation:this.addpostForm.get("location")?.value,
+          postPrice:this.addpostForm.get("price")?.value,
+          postType:this.addpostForm.get("type")?.value,
+          userEmail:this.addpostForm.get("email")?.value,
+          userPhonenumber:this.addpostForm.get("mobile")?.value,
+          postDetails:this.addpostForm.get("details")?.value,
+          postImages:this.imgname
+        }).subscribe()
+
+
+
+
+        alert("sended")
+
+      }
   
 }
--- kochin-bazzar-app/src/app/register/register.component.ts
+++ kochin-bazzar-app/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  Userform = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    location: new FormControl(''),
+    usertype: new FormControl({value:'User',disabled:true}),
+    mobile: new FormControl(''),
+    password: new FormControl(''),
+    cpassword: new FormControl('')
+    
+  });
+
+  Signup(){
+    console.log(this.Userform.get('firstName')?.value)
+
+
+    this.http.post("http://kochinuser-env.eba-r4yux4kr.ap-northeast-1.elasticbeanstalk.com/users/adduser",{
+          userName:this.Userform.get("firstName")?.value,
+          userLastname:this.Userform.get("lastName")?.value,
+          userEmail:this.Userform.get("email")?.value,
+          userPhonenumber:this.Userform.get("mobile")?.value,
+          userLocation:this.Userform.get("location")?.value,
+          userType:this.Userform.get("usertype")?.value,
+          userPassword:this.Userform.get("password")?.value
+        }).subscribe(data=>console.log(data))
+
+
   }
 
 }
